32468ecbbd8469c749138a48a0d7611af35f8c76
wertusser <wertusser@gmail.com>
2019-04-24 00:05:41 +0500

    Updates

---
+++ precache-manifest.b77a32bfeaab5fee20d145b22916aa14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761e8c225f36687f4a13ea6527c68ba0",
+    "url": "/ganlery/index.html"
+  },
+  {
+    "revision": "29363f2f75153035b11f",
+    "url": "/ganlery/static/css/main.19840e20.chunk.css"
+  },
+  {
+    "revision": "2a16b950e978ac0ad6c9",
+    "url": "/ganlery/static/js/2.6b702d8f.chunk.js"
+  },
+  {
+    "revision": "29363f2f75153035b11f",
+    "url": "/ganlery/static/js/main.df7a7b0c.chunk.js"
+  },
+  {
+    "revision": "5a6dbc7a7a9d517d23e3",
+    "url": "/ganlery/static/js/runtime~main.7fa2d085.js"
+  }
+]);
