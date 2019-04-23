f94d0cc25e8b6106daf2029e0079a9734e8ddc63
wertusser <wertusser@gmail.com>
2019-04-24 00:04:27 +0500

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash';
+
+function format(string) {
+  string = string.replace(/<unk>/g, _.repeat("█", _.random(4, 8)));
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+class Artwork extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { url: "", description: "" };
+  }
+
+  componentWillMount() {
+    fetch("https://artwork-doesnt-exists.firebaseio.com/images.json")
+    .then(data => data.json())
+    .then(data => _.sample(data))
+    .then(state => this.setState(state))
+  }
+
+  render() {
+    const art = {backgroundImage: 'url(' + this.state.url + ')'}
+    return (
+    <div>
+      <div className="art" style={art} alt={"Artwork " + this.state.description}></div>
+      <h1>"{format(this.state.description)}"</h1>
+    </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Artwork />
+    </main>
   );
 }
 
